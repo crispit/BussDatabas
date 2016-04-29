43fd2e0881e3e485acbfbd42c59f77d97902f5a0
andreas-a <andreas-andersson08@live.com>
2016-04-29 15:25:20 +0200

    Färgskala med kanppar som kan markeras. Men ännu inte gjort så att alla andra knappar avmarkeras när en markeras.

--- app/src/main/java/com/example/fredrikhansson/komigennuraa/MainActivity.java
+++ app/src/main/java/com/example/fredrikhansson/komigennuraa/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fredrikhansson.komigennuraa;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
+import android.hardware.camera2.params.BlackLevelPattern;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,8 @@ import java.util.ArrayList;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     DbHelper mydb;
@@ -26,13 +30,25 @@ public class MainActivity extends AppCompatActivity {
 
     //Method for showing all error reports in the database via a button
     public void displayReport (View V){
-            Intent i = new Intent(this, TestList.class);
+        Intent i = new Intent(this, TestList.class);
             startActivity(i);
     }
 
     //Method for adding reports in the database via a button
-    public void addReport (View V){
-            mydb.insertErrorReport("b", "a", "a", "a", "b", 1);
+    public void addReport (View v){
+        mydb.insertErrorReport("b", "a", "a", "a", "b", 1);
+    }
+
+    public void selectColor (View v) {
+        v.setSelected(true);
+
+            //int c1 = v.getSolidColor();
+        //v.setBackgroundResource(R.drawable.border_button);
+
+        //v.setBackgroundColor(c1);
+
+        //Button B = (Button) findViewById(R.id.)
+
     }
 
     public void testTable (){
